e9a452d438f83d10a74a6f5e5e2d4bfe77c5b81e
Cristopher <alancristophercr@gmail.com>
2022-07-05 14:25:56 -0500

    rama clase 4 y tarea

---
+++ practicas/tarea1cls04.js
@@ -0,0 +1,15 @@
+
+// Tomar un string del usaurio
+// imprimir en consola el numero de caracteres que tiene esa cadea *
+// Imprimir cuantas vocales estan presentes *
+// Imprimri cada uno de los las palabras que hay ('hola koder') => ['hola', 'koders'}*
+// Leer la documentacion de metodos de string
+// Hacer uno o mas pruebas  con algunos de los metodos leidos (presentar en clase)
+
+let consult = prompt('Ingresa tu cancion favorita:');
+let vocales = consult.match(/[aeiou]/gi)
+let result = consult.split(" ");
+let remplace =consult.replaceAll(/[aeiou]/gi,"i")
+console.log(`tu cancion favorita es: ${result}\ncantidad de vocales: ${vocales.length}\ncantidad de caracteres: ${consult.length}`);
+console.log(consult.trim())//este remueve los espacios en blanco al principio y final de la consulta
+console.log(remplace);//ejercicios adicionales
