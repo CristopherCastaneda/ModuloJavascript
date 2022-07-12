5e78bf939fed9b549791d81305600f98134da9dd
Cristopher <alancristophercr@gmail.com>
2022-07-12 17:37:36 -0500

    avances

--- practicas/clase08/tarea.js
+++ practicas/clase08/tarea.js
@@ -9,27 +9,18 @@
 // * Pista / Hint, debemos iterar el array (iterar = ${})
 // }
 
-const arr = [1,2,3,4,18]
-const pract = (pares) => {
-let cubo ="";
-for(i=0;i<arr.length;i++){
-    cubo += (arr[i]%2) == 0 ? cubo
-}return cubo
-}
-console.log(pract(arr));
-
-arr = [1,2,3,4,18]
-i=(5)
-
-cubo = (arr[1]%2) == 0 false (i++)
-cubo = (arr[2]%2) == 0 true ()
-
-
-
-
-
-
-
+// const arrayNumbers= [6, 8, 10, 7, 12, 7, 4, 10];
+// const functionNumbers = (array)=>{
+//     let result=[]
+//         for(i=0; i<array.length; i++){
+//             if(array[i] %2===0){
+//                 result.push(array[i])
+//             }
+//         }
+//         return result;
+// }
+// let result=functionNumbers(arrayNumbers)
+// console.log(result)
 
 // * Ejercicio 2 *Opcional
 // * Función que reciba como parámetro una array de strings
@@ -38,3 +29,52 @@ cubo = (arr[2]%2) == 0 true ()
 // * -> firstAndLast ( ['hola', 'mundo'] ) -> ['ha', 'mo']
 // *
 // * Pista / Hint, debemos iterar el array
+
+
+// Ejercicio 1 
+// * Función que reciba una palabra
+// * Retorna la palabra invertida
+// * La reversa del string lo haremos usando métodos de array
+// *  hint :  a un string no podemos aplicarle métodos array pero hay métodos de string que nos devuelve un array
+// *
+// * reverseStr('hola mundo')
+// * -> 'odnum aloh'
+
+// let capitci = prompt("palabra")
+// const reversa = (usuario) => {
+//     return Array.from(usuario).reverse().join("");
+// }
+// console.log(reversa(capitci));
+
+// Ejercicio 2 
+// * Dado el siguiente arreglo de ciudades
+// *   ['méXicO', 'PErÚ', 'eSpAña', 'inGlaterrA']
+// * Generar una función que capitalize cada elemento del array
+// * => Se puede resolver con .forEach() o .map()
+
+const capitalize = (spaceholder) => {                                // esta es una funcion lo que esta en parentesis es el space holder
+    let capCities = [];                                      // se asigna una variabla para colocar el resultado se deja en blanco con estilo array []
+    spaceholder.forEach((city) => {                                  // (spaceholder.forEach) = se le asigna al spaceholder el metodo de for each y le asignas  ((otro nombre))                       
+       if(city.length>2)
+        let cityCapitalized = `${city[0].toUpperCase()}${city       // esto es un  
+        .slice(1)
+        .toLowerCase()}`;
+  
+      capCities.push(cityCapitalized);
+    });
+  
+    return capCities;
+  };
+  console.log(capitalize(["méxicO", "RIo", "Los AngelEs"]));
+
+
+
+// Ejercicio 3
+// * Dado un arreglo con nombres de personas,
+// * Función que devuelva un arreglo con los nombres de las personas que empiezan con vocales y además capitalizados
+// *
+// * onlyNamesVowels( ['jorge','ana','ivan','sergio','oscar' ] )
+// * -> ['Ana','Ivan','Oscar']
+// *
+// * => Se puede resolver con .forEach() o .filter() stringmetodo search
+// */
