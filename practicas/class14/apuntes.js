046bb1bdc737288af711f8b16bb2cc47922d875e
Cristopher <alancristophercr@gmail.com>
2022-07-24 23:23:31 -0500

    funciones

---
+++ practicas/class14/apuntes.js
@@ -0,0 +1,81 @@
+// ! eventos
+// !estos suceden sobre los elementos de nuestro dom y podemos capturar y reaccionara
+// ellos, manipularlos a nuestro favor
+
+//! tipos de eventos
+    //! eventos automaticos
+    //! eventos generados por usuarios
+//-! eventos en linea ->Online Events.
+
+//! eventlistener
+// ses un metodo de los elementos del dom que nos permite agregar elementos o oyentes a nuestros elementos
+
+// document.querySelector('.btn')
+// btn1.addEvenListener('click')
+// ! Eventos 
+    // ! Estos suceden sobre los elementos de nuestro dom y podemos capturar y reaccionar a ellos, manipularlos a nustros favor 
+
+    //! tipos de enventos 
+        // ! Eventos Automaticos 
+        // ! Eventos generados por usaurios 
+
+
+// ! Eventos en linea -> Online events 
+
+
+// const funEvent = () => {
+// ! Demo change background
+// console.log('Estoy cambiando')
+//     document.body.style.backgroundColor = `
+//     rgb(
+//         ${Math.floor(Math.random() * (255+1))},
+//         ${Math.floor(Math.random() * (255+1))},
+//         ${Math.floor(Math.random() * (255+1))})
+//  `
+// }
+
+
+// //! Eventos mas ussados
+// //? click
+// //? change
+// //? input
+// //? focus
+// //? blur
+
+// //! Eventos de mouse
+// //? click
+// //? contextmenu
+// //? dbclick
+// //? mousedown
+// //? mouseup
+// //? mouseenter
+// //? mouseleave
+// //? mousemove
+// //? mouseover
+
+// //! Eventos de teclado
+// //? keydown
+// //? keypress
+// //? keyup
+
+// //! Eventos de navegador
+// //? load
+// //? scroll
+
+ // ! EventListener 
+// ! Es un metodo de los elementos del dom
+
+// let btn1 = document.querySelector('.btn1')
+
+// btn1.addEventListener('submit', funEvent)
+
+ // ! event handlers 
+
+// ! Event object 
+
+// let btn1 = document.querySelector('.btn1')
+// console.log(btn1.value)
+
+// btn1.addEventListener('click',  (e) => {
+//     console.log(e)
+// })
